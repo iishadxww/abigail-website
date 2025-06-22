1a3aa06b1dc43dd110994ec5aa6c2670e928e376
Abigail <carecorner.health@gmail.com>
2025-06-22 14:41:42 -0400

    modified the vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/abigail-website/', // 👈 VERY IMPORTANT: match your GitHub repo name
 })
